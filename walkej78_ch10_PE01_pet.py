3d68c814cda00f111120136b2984204c37f632b7
jw-upmi <101088270+jw-upmi@users.noreply.github.com>
2023-07-25 14:04:12 -0400

    write basic main and import pet class from walkej78_ch10_PE01_pet.py

---
+++ walkej78_ch10_PE01_pet.py
@@ -0,0 +1,31 @@
+# define pet class for walkej78_ch10_PE01_pet.py
+class Pet:
+    # initialize private methods
+    def __init__(self):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+
+    # set_name This method assigns a value to the __name field.
+    def set_name(name):
+        name = self.__name
+        
+    # set_animal_type This method assigns a value to the __animal_type field.
+    def set_animal_type(animal_type):
+        animal_type = self.__animal_type
+        
+    # set_age This method assigns a value to the __age field.
+    def set_age(age):
+        age = self.__age
+        
+    # get_name This method assigns a value to the __name field.
+    def get_name():
+        return self.__name
+    
+    # get_animal_type This method assigns a value to the __animal_type field.
+    def get_animal_type():
+        return self.__animal_type
+    
+    # get_age This method assigns a value to the __age field.
+    def get_age():
+        return self.__age
